2e8c815857313f1699991f2e18b0f891a3423e70
maikelfam <maikel.fam@kellogg.northwestern.edu>
2022-01-25 15:48:37 +0000

    did conditionals lab

--- labs/2-conditionals.rb
+++ labs/2-conditionals.rb
@@ -25,3 +25,20 @@
 # "i would like #{5} tacos"
 # We can specify multiple conditions with || (OR) and && (AND)
 # if dinner == "tacos" || dinner == "pizza"
+
+first_roll = rand(1..6)
+second_roll = rand(1..6)
+puts "first roll = #{first_roll}"
+puts "second roll = #{second_roll}"
+
+#create variable for total and print to screen
+total = first_roll + second_roll
+puts "total is #{total}"
+
+if total == 7 || total == 11
+    puts "YOU WIN!"
+elsif total == 2 || total == 3 || total == 12
+    puts "YOU LOSE"
+else 
+    puts "THE POINT IS #{total}"
+end 
